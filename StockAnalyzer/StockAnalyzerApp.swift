a125425cfb32150b26dada7fd81af27e45c9f8e6
Dávid Halascsák <david.halascsak@gmail.com>
2023-02-01 17:46:15 +0100

    Initial Commit

---
+++ StockAnalyzer/StockAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAnalyzerApp.swift
+//  StockAnalyzer
+//
+//  Created by Dávid Halascsák on 2023. 02. 01..
+//
+
+import SwiftUI
+
+@main
+struct StockAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

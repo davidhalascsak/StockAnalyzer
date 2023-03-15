ba18bac8890debba3e4088e34119efe5c51e711f
Dávid Halascsák <david.halascsak@gmail.com>
2023-03-15 09:28:03 +0100

    Remove the tracking of files.

--- StockAnalyzer/Utilities/ApiKeys.swift
+++ StockAnalyzer/Utilities/ApiKeys.swift
@@ -3,5 +3,4 @@ import Foundation
 struct ApiKeys {
     static let financeApi = "155b0ad8043a4eb3b270ae7df0c480d7" // d5f365f0f57c273c26a6b52b86a53010
     static let newsApi = "kz5np7ripsrsc0540ahrnmeep5r2uusbazhyeqyr"
-    static let firebaseApi = ""
 }

4bb75ac6b3722bb2e9a00501f162fd69e4ba5e9c
Dávid Halascsák <david.halascsak@gmail.com>
2023-03-25 22:12:14 +0100

    Update FinancialModel.

---
+++ StockAnalyzer/Models/FinanceModel.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+struct IncomeStatement: Codable, Hashable {
+    let date: String
+    let reportedCurrency: String
+    let revenue: Int
+    let grossProfit: Int
+    let operatingIncome: Int
+    let incomeBeforeTax: Int
+    let incomeTaxExpense: Int
+    let netIncome: Int
+    let weightedAverageShsOut: Int
+}
+
+// Total Assets    -    Accounts Payable & Accrued Expense    -    ( Cash, Cash Equivalents, Marketable Securities    -    max(0, Total Current Liabilities    -    Total Current Assets    +    Cash, Cash Equivalents, Marketable Securities    ))
+
+struct BalanceSheet: Decodable {
+    let date: String
+    let reportedCurrency: String
+    
+    let cashAndCashEquivalents: Int
+    let shortTermInvestments: Int
+    let netReceivables: Int
+    let inventory: Int
+    let otherCurrentAssets: Int
+    let totalCurrentAssets: Int
+    
+    let longTermInvestments: Int
+    let propertyPlantEquipmentNet: Int
+    let goodwill: Int
+    let intangibleAssets: Int
+    let otherNonCurrentAssets: Int
+    let totalNonCurrentAssets: Int
+    
+    let accountPayables: Int
+    let shortTermDebt: Int
+    let otherCurrentLiabilities: Int
+    let totalCurrentLiabilities: Int
+    
+    let longTermDebt: Int
+    let otherNonCurrentLiabilities: Int
+    let totalNonCurrentLiabilities: Int
+    
+    let totalInvestments: Int
+    let totalDebt: Int
+    let netDebt: Int
+    
+    var totalAssets: Int {
+        return self.totalCurrentAssets + self.totalNonCurrentAssets
+    }
+    
+    var totalLiabilities: Int {
+        return self.totalCurrentLiabilities + self.totalNonCurrentLiabilities
+    }
+}
+
+struct CashFlowStatement: Decodable {
+    let date: String
+    let reportedCurrency: String
+    let netIncome: Int
+    let netCashProvidedByOperatingActivities: Int
+    let netCashUsedForInvestingActivites: Int
+    let netCashUsedProvidedByFinancingActivities: Int
+    
+    let stockBasedCompensation: Int
+    let debtRepayment: Int
+    let commonStockIssued: Int
+    let commonStockRepurchased: Int
+    let dividendsPaid: Int
+    
+    let netChangeInCash: Int
+    let cashAtBeginningOfPeriod: Int
+    let cashAtEndOfPeriod: Int
+    
+    let operatingCashFlow: Int
+    let capitalExpenditure: Int
+    let freeCashFlow: Int
+}
+
